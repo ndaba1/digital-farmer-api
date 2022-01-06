b6c4ca8349557bbdbab89a649a411a3a8cbc28b4
Victor Ndaba <mwangindaba@gmail.com>
2022-01-06 22:23:49 +0300

    commit: setup routes correctly

--- api/api.v1.js
+++ api/api.v1.js
@@ -1,9 +1,11 @@
 import express from "express";
 
+import plantsRoute from "../routes/plants/plants.route.js";
+import diseasesRoute from "../routes/diseases/diseases.route.js";
+
 const router = express.Router();
 
-router.get("/test", (req, res) => {
-  res.send("Okay");
-});
+router.use("/plants", plantsRoute);
+router.use("/diseases", diseasesRoute);
 
 export default router;
