4b99dd01fc7fd127ad1fc6fafdbd82cc115f20cb
Victor Ndaba <mwangindaba@gmail.com>
2022-01-08 21:52:33 +0300

    commit: added loadplantdata function

--- server.js
+++ server.js
@@ -4,16 +4,16 @@ dotenv.config();
 
 import app from "./app.js";
 import connectToDb from "./services/dbConnection.js";
-import readCsvData from "./services/csvParse.js";
 import log from "./services/utils.js";
+import { loadPlantsData } from "./models/plants.model.js";
 
 const PORT = process.env.PORT || 9000;
 const server = http.createServer(app);
 
-connectToDb.then(() => {
-  readCsvData.then(() => {
-    server.listen(PORT, () => {
-      log.debug(`Server listening on port: ${PORT}`);
-    });
+connectToDb.then(async () => {
+  await loadPlantsData();
+
+  server.listen(PORT, () => {
+    log.debug(`Server listening on port: ${PORT}`);
   });
 });
