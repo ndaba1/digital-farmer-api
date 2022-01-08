3eb0f7834e42ace1286ffe3b623899b935e00d8f
Victor Ndaba <mwangindaba@gmail.com>
2022-01-08 20:04:36 +0300

    fix: patched verifyToken function

--- middleware/token.js
+++ middleware/token.js
@@ -2,6 +2,8 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
 
+import log from "../services/utils.js";
+
 export async function generateAccessToken() {
   const token = jwt.sign(
     {
@@ -16,14 +18,17 @@ export async function generateAccessToken() {
   return token;
 }
 
-export async function verifyToken() {
+export async function verifyToken(req, res, next) {
   const authHeader = req.headers["x-auth-token"];
   if (!authHeader) return res.status(401).json({ msg: "No token present" });
 
   const token = authHeader.split(" ")[1];
 
+  if (!token) return res.status(401).json({ msg: "Token header is invalid" });
+
   jwt.verify(token, process.env.JWT_SECRET, (error, user) => {
     if (error) {
+      log.debug(error);
       return res.status(401).json({ msg: "Token is invalid" });
     }
     req.user = user;
