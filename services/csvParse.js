7218fe4bffb6cd3d00077963e9f825f74c97165e
Victor Ndaba <mwangindaba@gmail.com>
2022-01-08 22:22:52 +0300

    fix: changed services into functions that return promises

--- services/csvParse.js
+++ services/csvParse.js
@@ -7,28 +7,30 @@ const __dirname = path.resolve();
 
 const results = [];
 
-const readCsvData = new Promise((resolve, reject) => {
-  fs.createReadStream(path.resolve(__dirname, ".", "data/mock_data.csv"))
-    .pipe(
-      parse({
-        comment: "#",
-        columns: true,
+async function readCsvData() {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(path.resolve(__dirname, ".", "data/mock_data.csv"))
+      .pipe(
+        parse({
+          comment: "#",
+          columns: true,
+        })
+      )
+      .on("data", (chunk) => {
+        results.push(chunk);
       })
-    )
-    .on("data", (chunk) => {
-      results.push(chunk);
-    })
-    .on("error", (err) => {
-      log.error(err);
-      reject(err);
-    })
-    .on("end", () => {
-      log.debug("Done parsing CSV data...");
-      // log.debug(results);
-      // log.debug(parseCSV(results));
-      resolve(parseCSV(results));
-    });
-});
+      .on("error", (err) => {
+        log.error(err);
+        reject(err);
+      })
+      .on("end", () => {
+        log.debug("Done parsing all the CSV data...");
+        // log.debug(results);
+        // log.debug(parseCSV(results));
+        resolve(parseCSV(results));
+      });
+  });
+}
 
 function parseCSV(arr) {
   let parsedData = [];
--- services/dbConnection.js
+++ services/dbConnection.js
@@ -4,22 +4,24 @@ dotenv.config();
 
 import log from "../services/utils.js";
 
-const connectToDb = new Promise((resolve, reject) => {
-  mongoose.connect(
-    process.env.MONGO_URI,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      autoIndex: false,
-    },
-    (err) => {
-      if (err) reject(err);
-      else {
-        log.debug("Connected to DB");
-        resolve();
+function connectToDb() {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(
+      process.env.MONGO_URI,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        autoIndex: false,
+      },
+      (err) => {
+        if (err) reject(err);
+        else {
+          log.debug("Connected to DB");
+          resolve();
+        }
       }
-    }
-  );
-});
+    );
+  });
+}
 
 export default connectToDb;
