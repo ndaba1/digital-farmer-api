c1bcb897e418b253741a7085201ca2657efa187c
Victor Ndaba <mwangindaba@gmail.com>
2022-01-12 19:26:35 +0300

    commit: updated diseases resolver

---
+++ graphql/diseases/diseases.resolver.js
@@ -0,0 +1,10 @@
+export default {
+  Query: {
+    diseases: async () => {
+      // Implement disease functionality
+    },
+    disease: async () => {
+      // Implement disease functionality
+    },
+  },
+};
