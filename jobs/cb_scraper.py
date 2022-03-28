b68dde414c86bbdf5eb4f786dccbce0d4fb1b013
Victor Ndaba <mwangindaba@gmail.com>
2022-03-28 15:35:40 +0300

    📦 feat: cb_scraper extracts actual data

--- jobs/cb_scraper.py
+++ jobs/cb_scraper.py
@@ -1,24 +1,47 @@
 
+from ast import Str
 from utils import get_page_html
 from bs4 import BeautifulSoup
 
 
 class CbScrapper:
-    def __init__(self, base_url, headers) -> None:
-        self.base_url = base_url
+    def __init__(self, links, headers) -> None:
         self.headers = headers
-        self.links = []
+        self.links = links
 
     def start(self):
-        main_page = get_page_html(self.base_url, self.headers)
-        self.extract_links(main_page)
+        for link in self.links:
+            main_page = get_page_html(link, self.headers)
+            disease = CbDiseaseGenerator(main_page)
 
-    def extract_links(self, html):
+
+class CbDiseaseGenerator():
+    def __init__(self, html) -> None:
         soup = BeautifulSoup(html, 'lxml')
-        links = soup.find_all("a", attrs={
-            "class": "link-reset"
-        })
+        self.soup = soup
 
-        for link in links:
-            if link.attrs['href'].__contains__("infos"):
-                self.links.append(link.attrs['href'])
+        self.start()
+
+    def start(self):
+        # get disease identity
+        identity = self.soup.find('div', id='toidentity')
+        titles = identity.find_all('h4')
+
+        for (i, title) in enumerate(titles):
+            # returns a <ul>
+            data = title.find_next_sibling()
+            items = data.find_all('li')
+
+            if len(items) == 1:
+                value = items[0].text.strip().replace("\n", " ")
+            else:
+                value = []
+                for item in items:
+                    data = item.text.strip().replace("\n", " ")
+                    # temp = f'{data[0].strip()}: ({data[1].strip()})'
+                    value.append(data)
+                    # print(data)
+
+            print({
+                title.text: value
+            })
