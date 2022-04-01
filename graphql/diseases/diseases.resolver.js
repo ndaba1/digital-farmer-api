352ef456cc0872c4bb26ca654c33bdc2cb62f4a3
Victor Ndaba <mwangindaba@gmail.com>
2022-04-01 16:49:43 +0300

    📦 feat: updated the resolvers for the diseases query

--- graphql/diseases/diseases.resolver.js
+++ graphql/diseases/diseases.resolver.js
@@ -1,10 +1,23 @@
+import { gQLVerifyToken } from "../../middleware/token";
+import { getAllDiseases, getDiseaseByName } from "../../models/diseases.model";
+
 export default {
   Query: {
-    diseases: async () => {
-      // Implement disease functionality
+    diseases: async (_parent, _args, ctx, _info) => {
+      const user = await gQLVerifyToken(ctx);
+
+      if (user.verified) {
+        const data = await getAllDiseases();
+        return data;
+      }
     },
-    disease: async () => {
-      // Implement disease functionality
+    disease: async (_parent, args, ctx, _info) => {
+      const user = await gQLVerifyToken(ctx);
+
+      if (user.verified) {
+        const data = await getDiseaseByName(args.name);
+        return data;
+      }
     },
   },
 };
