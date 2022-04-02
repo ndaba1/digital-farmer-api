0c9bd12d2701ead023e805fd44650bd7f41fe139
Victor Ndaba <mwangindaba@gmail.com>
2022-04-02 19:11:55 +0300

    🧹 chore: added config for the cb_scraper

--- jobs/config.py
+++ jobs/config.py
@@ -8,3 +8,6 @@ headers = {
     "Content-Type": "application/json",
     "User-Agent": config["USER-AGENT"]
 }
+
+cabi = [
+    'http://127.0.0.1:5500/Puccinia%20sorghi%20(common%20rust%20of%20maize).html']
--- jobs/main.py
+++ jobs/main.py
@@ -1,6 +1,9 @@
+from cb_scraper import CbScrapper
 from pv_scraper import PvScraper
-from config import config, headers
+from config import config, headers, cabi
 
 if __name__ == "__main__":
-    s = PvScraper(config['LOCAL_TEST_URL'], headers)
+    # s = PvScraper(config['LOCAL_TEST_URL'], headers)
+    # s.start()
+    s = CbScrapper(cabi, headers)
     s.start()
