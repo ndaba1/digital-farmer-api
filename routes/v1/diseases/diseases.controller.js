66d5e3839c080e267417cca944ec10da829f6504
Victor Ndaba <mwangindaba@gmail.com>
2022-04-02 19:15:08 +0300

    📦 feat: added route for posting diseases & get targets fn

--- routes/v1/diseases/diseases.controller.js
+++ routes/v1/diseases/diseases.controller.js
@@ -1,13 +1,15 @@
 import {
   getAllDiseases,
   getDiseaseByName,
+  getDiseaseTargets,
+  postNewDisease,
 } from "../../../models/diseases.model";
 
 export async function httpGetAllDiseases(req, res) {
   const user = req.user;
 
   if (user.verified) {
-    const data = await getAllDiseases(req.params.id);
+    const data = await getAllDiseases();
     return res.status(200).json(data);
   } else {
     return res.status(400).json({ msg: "That's a bad request kemosabe" });
@@ -17,10 +19,40 @@ export async function httpGetAllDiseases(req, res) {
 export async function httpGetDisease(req, res) {
   const user = req.user;
 
-  if (user.verified) {
-    const data = await getDiseaseByName(req.params.id);
-    return res.status(200).json(data);
+  if (user.verified && req.params.name) {
+    const data = await getDiseaseByName(req.params.name);
+    if (data) return res.status(200).json(data);
+
+    return res.status(404).json({ msg: "The requested disease was not found" });
   } else {
     return res.status(400).json({ msg: "That's a bad request kemosabe" });
+  }
+}
+
+export async function httpGetDiseaseTargets(req, res) {
+  const user = req.user;
+
+  if (user.verified && req.params.name) {
+    const data = await getDiseaseTargets(req.params.name);
+    if (data) return res.status(200).json(data);
+
+    return res.status(404).json({ msg: "The requested disease was not found" });
+  } else {
+    return res.status(400).json({ msg: "That's a bad request kemosabe" });
+  }
+}
+
+export async function httpPostDisease(req, res) {
+  const user = req.user;
+
+  if (user.verified && user.isAdmin) {
+    if (req.body.data) {
+      await postNewDisease(req.body.data);
+      return res.status(200).json({ msg: "Success" });
+    } else {
+      return res.status(400).json({ msg: "Payload cannot be empty" });
+    }
+  } else {
+    return res.status(401).json({ msg: "That's unauthorized kemosabe" });
   }
 }
--- routes/v1/diseases/diseases.route.js
+++ routes/v1/diseases/diseases.route.js
@@ -1,9 +1,23 @@
 import express from "express";
+import { verifyToken } from "../../../middleware/token";
+import {
+  httpGetAllDiseases,
+  httpGetDisease,
+  httpGetDiseaseTargets,
+  httpPostDisease,
+} from "./diseases.controller";
 
 const router = express.Router();
 
 router.get("/test", (req, res) => {
   res.send("Okay!");
 });
+
+router.get("/", verifyToken, httpGetAllDiseases);
+
+router.get("/:name", verifyToken, httpGetDisease);
+router.get("/:name/targets", verifyToken, httpGetDiseaseTargets);
+
+router.post("/", verifyToken, httpPostDisease);
 
 export default router;
