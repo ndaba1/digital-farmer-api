6cda82663f0331ad6bbed0243b25fd63af212160
Victor Ndaba <mwangindaba@gmail.com>
2022-11-17 12:36:21 +0300

    refactor: updated app entrypoint

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
-import { graphqlHTTP } from "express-graphql";
+// import { graphqlHTTP } from "express-graphql";
 
 import v1Route from "./api/api.v1.js";
-import schema from "./graphql/index.js";
+// import schema from "./graphql/index.js";
 
 const app = express();
 
@@ -18,11 +18,11 @@ app.use(morgan("common"));
 
 app.use("/v1", v1Route);
 
-app.use(
-  "/v1/query",
-  graphqlHTTP({
-    schema,
-  })
-);
+// app.use(
+//   "/v1/query",
+//   graphqlHTTP({
+//     schema,
+//   })
+// );
 
 export default app;
